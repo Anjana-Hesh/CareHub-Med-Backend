1afe2d1ed237f9eb6bb666f8953456dd067dbe51
LENOVO <anjanaheshan676@gmail.com>
2025-11-14 13:08:24 +0530

    doctor login api created

--- src/controllers/doctorController.ts
+++ src/controllers/doctorController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express"
 import doctorModel from "../models/doctorModel"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const changeAvailability = async (req: Request,resp: Response) => {
     try {
@@ -38,6 +40,42 @@ export const doctorList = async (req: Request,resp: Response) => {
             message:error.message
         })
      }
+}
+
+// API for doctor login
+export const loginDoctor = async (req: Request,resp: Response) => {
+
+    try {
+
+        const { email , password} = req.body
+        const doctor = await doctorModel.findOne({email})
+
+        if (!doctor) {
+            return resp.json({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, doctor.password)
+        if (isMatch) {
+            
+            const token = jwt.sign({id:doctor._id} , process.env.JWT_SECRET as string)
+
+            resp.json({success: true, token})
+
+        } else {
+            resp.json({success: false , message: " Invalid credentials"})
+        }
+        
+    } catch (error:any) {
+        console.log(error)
+        resp.status(500).json({
+            success: false,
+            message:error.message
+        })
+    }
+
 }
 
 // export default {changeAvailability , doctorList};
