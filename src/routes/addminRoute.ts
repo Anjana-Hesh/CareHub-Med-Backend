ed41c1d11aebd9f42931ab66627ce146cc6808d7
LENOVO <anjanaheshan676@gmail.com>
2025-11-21 23:16:29 +0530

    Change some endpoints

--- src/routes/addminRoute.ts
+++ src/routes/addminRoute.ts
@@ -7,7 +7,7 @@ import {changeAvailability }from '../controllers/doctorController'
 const addminRouter = express.Router()
 
 addminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
-addminRouter.post('/login', loginAdmin)
+// addminRouter.post('/login', loginAdmin)
 addminRouter.post('/all-doctors', authAdmin, allDoctors)
 addminRouter.post('/change-availability', authAdmin, changeAvailability)
 addminRouter.get('/appointments', authAdmin, appointmentAdmin)
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken"
+import { IUSER } from "../models/userModel"
 
 const JWT_SECRET = process.env.JWT_SECRET as string
 
-
-export const signAccessToken = (user): string => {
+export const signAccessToken = (user:IUSER): string => {
   return jwt.sign({ 
             sub: user._id.toString(), 
             roles: user.roles
